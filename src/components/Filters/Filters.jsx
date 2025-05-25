7cf3aadfc4f1c2af4d00a68ea4f4865e39112126
Iryna R <deutsche.post15@gmail.com>
2025-05-25 17:29:11 +0200

    fix

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -14,7 +14,7 @@ const Filters = () => {
     equipment: [],
     form: "",
   });
-  const spritePath = "/public/icons/symbol-defs.svg";
+  const spritePath = "/icons/symbol-defs.svg";
 
   // Опции фильтра
   const equipmentOptions = [
